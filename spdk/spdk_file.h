f74bbd2b6a4b4f34d5d70249e0875a5ca4c148ff
Ashlie Martinez <ashmrtnz@cs.washington.edu>
2019-10-11 14:41:58 -0700

    Fix define guard for spdk package.

--- spdk/spdk_file.h
+++ spdk/spdk_file.h
@@ -1,7 +1,7 @@
-#ifndef BBOLT_SPDK_FILE_H_
-#define BBOLT_SPDK_FILE_H_
+#ifndef BBOLT_SPDK_SPDK_FILE_H_
+#define BBOLT_SPDK_SPDK_FILE_H_
 
 // Trid should be in the form "trtype=<type> traddr=<addr>"
 int SpdkInit(const char *tridStr);
 
-#endif  // BBOLT_SPDK_FILE_H_
+#endif  // BBOLT_SPDK_SPDK_FILE_H_
